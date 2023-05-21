bf5a90a50185b3e29aa2a74716277caab35e7853
Nick Stanovic <nickstanovic@gmail.com>
2023-05-20 23:20:40 -0400

    adjust footer layout for better mobile appearance

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 const Footer = () => {
     return (
-        <footer className="md:container md:mx-auto grid grid-cols-4 bg-grass drop-shadow-container">
+        <footer className="md:container md:mx-auto grid grid-cols-2 bg-grass drop-shadow-container">
             <Image
                 src='/img/lucky-shrub-logo-footer.webp'
                 alt=''
@@ -11,7 +11,7 @@ const Footer = () => {
                 height={50}
                 className="place-self-center py-2"
             />
-            <p className="col-start-3 place-self-center">&copy; 2023 Lucky Shrub</p>
+            <p className="place-self-center">&copy; 2023 Lucky Shrub</p>
 
         </footer>
     )
