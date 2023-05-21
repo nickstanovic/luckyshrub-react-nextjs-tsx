8ccca9a4b11cc5078a69a69acb9d6b75b54c47c0
Nick Stanovic <nickstanovic@gmail.com>
2023-05-20 23:15:14 -0400

    prepare nav to handle nextjs routing

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,14 +1,24 @@
+import Link from 'next/link';
+
 const Nav = () => {
     return (
         <nav className="md:container md:mx-auto bg-grass text-center drop-shadow-nav py-2">
             <ul className="grid grid-cols-4">
-                <li>Home</li>
-                <li>Products</li>
-                <li>Contact</li>
-                <li>About</li>
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href="/products">Products</Link>
+                </li>
+                <li>
+                    <Link href="/contact">Contact</Link>
+                </li>
+                <li>
+                    <Link href="/about">About</Link>
+                </li>
             </ul>
         </nav>
     )
 }
 
-export default Nav
+export default Nav;
