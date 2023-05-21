cb92772f519cc771447feae70417876ad2825bb4
Nick Stanovic <nickstanovic@gmail.com>
2023-05-20 21:03:22 -0400

    add nav, no links configured yet

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import React, {FC} from 'react'
 import Footer from "@/components/Footer"
 import Header from "@/components/Header"
+import Nav from "@/components/Nav"
 
 interface Props {
   children: React.ReactNode
@@ -29,6 +30,7 @@ const RootLayout: FC<Props> = ({children}) => {
       <body className="text-white bg-green-800/30">
           <div className="md:container md:mx-auto min-h-screen drop-shadow-container bg-clay">
               <Header />
+              <Nav />
                 {children}
           </div>
         <Footer />
---
+++ components/Nav.tsx
@@ -0,0 +1,14 @@
+const Nav = () => {
+    return (
+        <nav className="md:container md:mx-auto bg-grass text-center drop-shadow-nav py-2">
+            <ul className="grid grid-cols-4">
+                <li>Home</li>
+                <li>Products</li>
+                <li>Contact</li>
+                <li>About</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Nav
