338c0ca8907de8e431aee4f52e33dcf6ca419f36
Nick Stanovic <nickstanovic@gmail.com>
2023-05-23 04:36:05 -0400

    update metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Welcome to Lucky Shrub',
+    description: 'Bring life to your living spaces with Lucky Shrub diverse collection of indoor and outdoor plants',
+}
+
 const Home = () => {
     return (
         <div>
--- components/Main.tsx
+++ components/Main.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 import Article from './Article';
 
 const Main: FC = () => {
