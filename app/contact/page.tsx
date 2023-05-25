07b3ad85b56f8fa638b1da6a3e8cc5ddc0bb33ab
Nick Stanovic <nickstanovic@gmail.com>
2023-05-25 08:25:37 -0400

    done with ui for mobile contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,45 +1,57 @@
 'use client'
 
 import React, {FC} from 'react'
+import Image from "next/image"
 
 const ContactForm: FC = () => (
-    <main className="bg-white dark:bg-gray-900">
-        <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact
-                Us</h2>
-            <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a
-                technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let
-                us know.</p>
-            <form action="#" className="space-y-8">
-                <div>
-                    <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your
-                        email</label>
-                    <input type="email" id="email"
-                           className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                           placeholder="name@flowbite.com" required />
+    <div className={"grid lg:grid-cols-3"}>
+        <main className="lg:col-span-3">
+            <Image
+                src="/img/lucky-shrub-contact.webp"
+                alt="About Lucky Shrub"
+                width={976}
+                height={800}
+                className="mx-auto py-6"
+            />
+            <h1 className="text-3xl lg:text-5xl text-center">Contact Us</h1>
+            <p className="lg:mx-10 p-4">Have a technical issue? Want to send feedback? Any questions at all? Maybe you just want to say hi? Let us know.</p>
+            <form action="#" className="space-y-8 mx-6 pt-2">
+                <div className="grid lg:grid-cols-5 mx-auto">
+                    <div className="lg:col-start-2">
+                        <label htmlFor="name" className="mb-1">&nbsp;Your Name
+                        </label><br/>
+                        <input type="name" id="name"
+                               placeholder=" Name"
+                               className="w-full"
+                               required/>
+                    </div><br/>
+                    <div className="lg:col-start-4 lg:justify-self-end">
+                        <label htmlFor="email" className="block mb-1">
+                            &nbsp;Your email
+                        </label>
+                        <input type="email"
+                               id="email"
+                               placeholder=" name@email.com"
+                               className="w-full"
+                               required/>
+                    </div><br/>
+                    <div className="lg:col-start-2">
+                        <label htmlFor="message" className="block mb-1">
+                             &nbsp;Your message
+                        </label>
+                        <textarea id="message"
+                                  rows={6}
+                                  className="block p-2.5 mb-3 w-full bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
+                                  placeholder="Leave a comment...">
+                        </textarea>
+                    </div>
+                    <button type="submit" className="py-2 px-5 mb-10 text-xl text-center text-white rounded-lg bg-grass  hover:bg-black focus:ring-4 focus:outline-none focus:ring-primary-300">
+                        Send message
+                    </button>
                 </div>
-                <div>
-                    <label htmlFor="subject"
-                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
-                    <input type="text" id="subject"
-                           className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-                           placeholder="Let us know how we can help you" required />
-                </div>
-                <div className="sm:col-span-2">
-                    <label htmlFor="message"
-                           className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your
-                        message</label>
-                    <textarea id="message" rows="6"
-                              className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                              placeholder="Leave a comment..."></textarea>
-                </div>
-                <button type="submit"
-                        className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send
-                    message
-                </button>
             </form>
-        </div>
-    </main>
+        </main>
+    </div>
 )
 
 export default ContactForm
