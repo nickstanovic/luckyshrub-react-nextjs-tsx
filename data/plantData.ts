97f50a9237db2d7be602c99e039e6fdf56484d36
Nick Stanovic <nickstanovic@gmail.com>
2023-05-27 00:26:46 -0400

    create file for products plant data

---
+++ data/plantData.ts
@@ -0,0 +1,88 @@
+export interface PlantData {
+    id?: string;
+    src: string;
+    alt: string;
+    title: string;
+    name: string;
+}
+
+export const greenShrubs: PlantData[] = [
+    {
+        src: "/img/products/Green-JewelAlocasia-Alocasia-Azlanii.webp",
+        alt: "Green JewelAlocasia Alocasia Azlanii",
+        title: "Green JewelAlocasia Alocasia Azlanii",
+        name: "Jewel Alocasia Azlanii",
+    },
+    {
+        src: "/img/products/Green-Mythic-Alocasia-Longiloba.webp",
+        alt: "Green Mythic Alocasia Longiloba",
+        title: "Green Mythic Alocasia Longiloba",
+        name: "Mythic Alocasia Longiloba",
+    },
+    {
+        src: "/img/products/Green-Mythic-ElephantEars-Alocasia-Baginda.webp",
+        alt: "Green Mythic ElephantEars Alocasia Baginda",
+        title: "Green Mythic ElephantEars Alocasia Baginda",
+        name: "Mythic Elephant Ears\nAlocasia Baginda",
+    },
+    {
+        src: "/img/products/Green-Mythic-JewelAlocasia-Alocasia-Baginda.webp",
+        alt: "Green Mythic JewelAlocasia Alocasia Baginda",
+        title: "Green Mythic JewelAlocasia Alocasia Baginda",
+        name: "Mythic Jewel\nAlocasia Baginda",
+    }
+]
+
+export const redShrubs: PlantData[] = [
+    {
+        src: "/img/products/Red-DoubleTake-Quince-Chaenomeles-Speciosa.webp",
+        alt: "Red DoubleTake Quince Chaenomeles Speciosa",
+        title: "Red DoubleTake Quince Chaenomeles Speciosa",
+        name: "DoubleTake Quince\nChaenomeles Speciosa",
+    },
+    {
+        src: "/img/products/Red-SimplyScentsational-Sweetshrub-Calycanthus-Floridus.webp",
+        alt: "Red SimplyScentsational Sweetshrub Calycanthus Floridus",
+        title: "Red SimplyScentsational Sweetshrub Calycanthus Floridus",
+        name: "SimplyScentsational Sweetshrub\nCalycanthus Floridus",
+    },
+    {
+        src: "/img/products/Red-SisterDisco-ScotchBroom-Cotinus-Coggygria.webp",
+        alt: "Red SisterDisco ScotchBroom Cotinus Coggygria",
+        title: "Red SisterDisco ScotchBroom Cotinus Coggygria",
+        name: "SisterDisco\nScotchBroom\nCotinus Coggygria",
+    },
+    {
+        src: "/img/products/Red-WinecraftBlack-Smokebush-Clematis.webp",
+        alt: "Red WinecraftBlack Smokebush Clematis",
+        title: "Red WinecraftBlack Smokebush Clematis",
+        name: "WinecraftBlack\nSmokebush\nClematis",
+    }
+]
+
+export const whiteShrubs: PlantData[] = [
+    {
+        src: "/img/products/White-Hardyabelia-Abelia-Xgrandiflora.webp",
+        alt: "White Hardyabelia Abelia Xgrandiflora",
+        title: "White Hardyabelia Abelia Xgrandiflora",
+        name: "Hardyabelia Abelia Xgrandiflora",
+    },
+    {
+        src: "/img/products/White-MuchoGusto-ButterflyBush-Abelia-Chinensis.webp",
+        alt: "White MuchoGusto ButterflyBush Abelia Chinensis",
+        title: "White MuchoGusto ButterflyBush Abelia Chinensis",
+        name: "MuchoGusto\nButterflyBush\nAbelia Chinensis",
+    },
+    {
+        src: "/img/products/White-RubyAnniversary-ButterflyBush-Abelia-Mosanensis.webp",
+        alt: "White RubyAnniversary ButterflyBush Abelia Mosanensis",
+        title: "White RubyAnniversary ButterflyBush Abelia Mosanensis",
+        name: "RubyAnniversary ButterflyBush\nAbelia Mosanensis",
+    },
+    {
+        src: "/img/products/White-SweetEmotion-ButterflyBush-Abelia-Grandiflora.webp",
+        alt: "White SweetEmotion ButterflyBush Abelia Grandiflora",
+        title: "White SweetEmotion ButterflyBush Abelia Grandiflora",
+        name: "SweetEmotion\nButterflyBush\nAbelia Grandiflora",
+    }
+]
