5763e50e2f6ed2a083f1ed46ab6fd5c4ec184584
Nick Stanovic <nickstanovic@gmail.com>
2023-06-01 12:47:46 -0400

    add color prop and dynamically color text based on prop

--- components/Plant.tsx
+++ components/Plant.tsx
@@ -6,13 +6,24 @@ type PlantProps = {
     alt: string;
     title: string;
     name: string;
+    color: 'green' | 'red' | 'white';
 };
 
-const Plant: FC<PlantProps> = ({src, alt, title, name}) => (
-    <div className="mb-10">
-        <Image className="mx-auto -mb-6 drop-shadow-image" src={src} alt={alt} title={title} width={273} height={298}/>
-        <p>{name}</p>
-    </div>
-)
+const Plant: FC<PlantProps> = ({src, alt, title, name, color}) => {
+    // Mapping plant colors to Tailwind CSS classes
+    const colorClasses = {
+        green: '',
+        red: 'text-red-600',
+        white: 'text-white', // White text might not be visible, so I'm using gray here
+    };
+    const textColor = colorClasses[color];
+
+    return (
+        <div className="mb-10">
+            <Image className="mx-auto -mb-6 drop-shadow-image" src={src} alt={alt} title={title} width={273} height={298}/>
+            <p className={textColor}>{name}</p>
+        </div>
+    )
+}
 
 export default Plant
--- data/plantData.ts
+++ data/plantData.ts
@@ -1,88 +1,97 @@
 export interface PlantData {
     id?: string;
     src: string;
+    name: string;
     alt: string;
     title: string;
-    name: string;
+    color: 'green' | 'red' | 'white';
 }
 
-export const greenShrubs: PlantData[] = [
+export const plants: PlantData[] = [
     {
         src: "/img/Green-JewelAlocasia-Alocasia-Azlanii.webp",
-        alt: "Green JewelAlocasia Alocasia Azlanii",
-        title: "Green JewelAlocasia Alocasia Azlanii",
         name: "Jewel Alocasia Azlanii",
+        alt: "green jewel alocasia azlanii",
+        title: "Green Jewel Alocasia Azlanii",
+        color: 'green',
     },
     {
         src: "/img/Green-Mythic-Alocasia-Longiloba.webp",
-        alt: "Green Mythic Alocasia Longiloba",
-        title: "Green Mythic Alocasia Longiloba",
         name: "Mythic Alocasia Longiloba",
+        alt: "green mythic alocasia longiloba",
+        title: "Green Mythic Alocasia Longiloba",
+        color: 'green',
     },
     {
         src: "/img/Green-Mythic-ElephantEars-Alocasia-Baginda.webp",
-        alt: "Green Mythic ElephantEars Alocasia Baginda",
-        title: "Green Mythic ElephantEars Alocasia Baginda",
         name: "Mythic Elephant Ears\nAlocasia Baginda",
+        alt: "green mythic elephant ears alocasia baginda",
+        title: "Green Mythic Elephant Ears Alocasia Baginda",
+        color: 'green',
     },
     {
         src: "/img/Green-Mythic-JewelAlocasia-Alocasia-Baginda.webp",
-        alt: "Green Mythic JewelAlocasia Alocasia Baginda",
-        title: "Green Mythic JewelAlocasia Alocasia Baginda",
         name: "Mythic Jewel\nAlocasia Baginda",
-    }
-]
-
-export const redShrubs: PlantData[] = [
+        alt: "green mythic jewel alocasia baginda",
+        title: "Green Mythic Jewel Alocasia Baginda",
+        color: 'green',
+    },
     {
         src: "/img/Red-DoubleTake-Quince-Chaenomeles-Speciosa.webp",
-        alt: "Red DoubleTake Quince Chaenomeles Speciosa",
-        title: "Red DoubleTake Quince Chaenomeles Speciosa",
         name: "DoubleTake Quince\nChaenomeles Speciosa",
+        alt: "red doubletake quince chaenomeles speciosa",
+        title: "Red DoubleTake Quince Chaenomeles Speciosa",
+        color: 'red',
     },
     {
         src: "/img/Red-SimplyScentsational-Sweetshrub-Calycanthus-Floridus.webp",
-        alt: "Red SimplyScentsational Sweetshrub Calycanthus Floridus",
-        title: "Red SimplyScentsational Sweetshrub Calycanthus Floridus",
         name: "SimplyScentsational Sweetshrub\nCalycanthus Floridus",
+        alt: "red simplyscentsational sweetshrub calycanthus floridus",
+        title: "Red SimplyScentsational Sweetshrub Calycanthus Floridus",
+        color: 'red',
     },
     {
         src: "/img/Red-SisterDisco-ScotchBroom-Cotinus-Coggygria.webp",
-        alt: "Red SisterDisco ScotchBroom Cotinus Coggygria",
-        title: "Red SisterDisco ScotchBroom Cotinus Coggygria",
         name: "SisterDisco\nScotchBroom\nCotinus Coggygria",
+        alt: "red sisterdisco scotchbroom cotinus coggygria",
+        title: "Red SisterDisco ScotchBroom Cotinus Coggygria",
+        color: 'red',
     },
     {
         src: "/img/Red-WinecraftBlack-Smokebush-Clematis.webp",
-        alt: "Red WinecraftBlack Smokebush Clematis",
-        title: "Red WinecraftBlack Smokebush Clematis",
         name: "WinecraftBlack\nSmokebush\nClematis",
-    }
-]
-
-export const whiteShrubs: PlantData[] = [
+        alt: "red winecraftblack smokebush clematis",
+        title: "Red WinecraftBlack Smokebush Clematis",
+        color: 'red',
+    },
     {
         src: "/img/White-Hardyabelia-Abelia-Xgrandiflora.webp",
-        alt: "White Hardyabelia Abelia Xgrandiflora",
-        title: "White Hardyabelia Abelia Xgrandiflora",
         name: "Hardyabelia Abelia Xgrandiflora",
+        alt: "white hardyabelia abelia xgrandiflora",
+        title: "White Hardyabelia Abelia Xgrandiflora",
+        color: 'white',
     },
     {
         src: "/img/White-MuchoGusto-ButterflyBush-Abelia-Chinensis.webp",
-        alt: "White MuchoGusto ButterflyBush Abelia Chinensis",
-        title: "White MuchoGusto ButterflyBush Abelia Chinensis",
         name: "MuchoGusto\nButterflyBush\nAbelia Chinensis",
+        alt: "white muchogusto butterflybush abelia chinensis",
+        title: "White MuchoGusto ButterflyBush Abelia Chinensis",
+        color: 'white',
     },
     {
         src: "/img/White-RubyAnniversary-ButterflyBush-Abelia-Mosanensis.webp",
-        alt: "White RubyAnniversary ButterflyBush Abelia Mosanensis",
-        title: "White RubyAnniversary ButterflyBush Abelia Mosanensis",
         name: "RubyAnniversary ButterflyBush\nAbelia Mosanensis",
+        alt: "white rubyanniversary butterflybush abelia mosanensis",
+        title: "White RubyAnniversary ButterflyBush Abelia Mosanensis",
+        color: 'white',
     },
     {
         src: "/img/White-SweetEmotion-ButterflyBush-Abelia-Grandiflora.webp",
-        alt: "White SweetEmotion ButterflyBush Abelia Grandiflora",
-        title: "White SweetEmotion ButterflyBush Abelia Grandiflora",
         name: "SweetEmotion\nButterflyBush\nAbelia Grandiflora",
+        alt: "white sweetemotion butterflybush abelia grandiflora",
+        title: "White SweetEmotion ButterflyBush Abelia Grandiflora",
+        color: 'white',
     }
-]
+];
+
+export default plants;
